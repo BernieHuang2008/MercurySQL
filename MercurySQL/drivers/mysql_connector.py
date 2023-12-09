06ec6717cbfac82ccfe7eaf847d2b70851e5f41e
berniehuang2008 <berniehuang2008@163.com>
2023-12-09 08:32:47 +0800

    Add MySQL Connector test workflow and test script

--- MercurySQL/drivers/mysql_connector.py
+++ MercurySQL/drivers/mysql_connector.py
@@ -31,51 +31,51 @@ class Driver_MySQLConnector(BaseDriver):
 
             @staticmethod
             def get_all_columns(table_name: str) -> str:
-                return f"DESCRIBE {table_name};"
+                return f"DESCRIBE `{table_name}`;"
 
             @staticmethod
             def create_table_if_not_exists(table_name: str, column_name: str, column_type: str, primaryKey=False, autoIncrement=False, engine='', charset='') -> str:
                 return f"""
-                    CREATE TABLE IF NOT EXISTS {table_name} (
-                        {column_name} {column_type} {'PRIMARY KEY' if primaryKey else ''} {'AUTO_INCREMENT' if autoIncrement else ''}
+                    CREATE TABLE IF NOT EXISTS `{table_name}` (
+                        `{column_name}` {column_type} {'PRIMARY KEY' if primaryKey else ''} {'AUTO_INCREMENT' if autoIncrement else ''}
                     ) {engine} {f'DEFAULT CHARSET={charset}' if charset else ''};
                 """
 
             @staticmethod
             def add_column(table_name: str, column_name: str, column_type: str) -> str:
-                return f"ALTER TABLE {table_name} ADD COLUMN {column_name} {column_type};"
+                return f"ALTER TABLE `{table_name}` ADD COLUMN `{column_name}` {column_type};"
 
             @staticmethod
             def drop_column(table_name: str, column_name: str) -> str:
-                return f"ALTER TABLE {table_name} DROP COLUMN {column_name};"
+                return f"ALTER TABLE `{table_name}` DROP COLUMN `{column_name}`;"
 
             @staticmethod
             def set_primary_key(table, keyname: str, keytype: str) -> list:
                 return [
-                    f"ALTER TABLE DROP PRIMARY KEY;",
-                    f"ALTER TABLE {table.table_name} ADD PRIMARY KEY ({keyname});"
+                    f"ALTER TABLE `{table.table_name}` DROP PRIMARY KEY;",
+                    f"ALTER TABLE `{table.table_name}` ADD PRIMARY KEY (`{keyname}`);"
                 ]
 
             @staticmethod
             def insert(table_name: str, columns: str, values: str) -> str:
-                return f"INSERT INTO {table_name} ({columns}) VALUES ({values});"
+                return f"INSERT INTO `{table_name}` ({columns}) VALUES ({values});"
             
             @staticmethod
             def insert_or_update(table_name: str, columns: str, values: str) -> str:
                 update_columns = ', '.join(f'{col}=VALUES({col})' for col in columns.split(', '))
-                return f"INSERT INTO {table_name} ({columns}) VALUES ({values}) ON DUPLICATE KEY UPDATE {update_columns};"
+                return f"INSERT INTO `{table_name}` ({columns}) VALUES ({values}) ON DUPLICATE KEY UPDATE {update_columns};"
 
             @staticmethod
             def update(table_name: str, columns: str, condition: str) -> str:
-                return f"UPDATE {table_name} SET {columns} WHERE {condition};"
+                return f"UPDATE `{table_name}` SET {columns} WHERE {condition};"
 
             @staticmethod
             def query(table_name: str, selection: str, condition: str) -> str:
-                return f"SELECT {selection} FROM {table_name} WHERE {condition};"
+                return f"SELECT {selection} FROM `{table_name}` WHERE {condition};"
 
             @staticmethod
             def delete(table_name: str, condition: str) -> str:
-                return f"DELETE FROM {table_name} WHERE {condition};"
+                return f"DELETE FROM `{table_name}` WHERE {condition};"
 
         @classmethod
         def get_all_tables(cls, conn) -> List[str]:
@@ -87,7 +87,8 @@ class Driver_MySQLConnector(BaseDriver):
         def get_all_columns(cls, conn, table_name: str) -> List[str]:
             cursor = conn.cursor()
             cursor.execute(cls.gensql.get_all_columns(table_name))
-            return list(map(lambda x: [x[0], x[1]], cursor.fetchall()))
+            # [name, type, null, key, default, extra]
+            return list(map(lambda x: [x[0], x[1], x[2], x[3], x[4], x[5]], cursor.fetchall()))
 
     class TypeParser:
         """
@@ -112,39 +113,11 @@ class Driver_MySQLConnector(BaseDriver):
             +-------------------+-------------+
             | int               | INTEGER     |
             +-------------------+-------------+
-            | int(1)            | TINYINT     |
-            +-------------------+-------------+
-            | int(2)            | SMALLINT    |
-            +-------------------+-------------+
-            | int(3)            | MEDIUMINT   |
-            +-------------------+-------------+
-            | int(4)            | INT         |
-            +-------------------+-------------+
-            | int(8)            | BIGINT      |
-            +-------------------+-------------+
             | float             | FLOAT       |
-            +-------------------+-------------+
-            | float(4)          | FLOAT       |
-            +-------------------+-------------+
-            | float(8)          | DOUBLE      |
             +-------------------+-------------+
             | str               | VARCHAR(225)|
             +-------------------+-------------+
-            | str('char')       | CHAR        |
-            +-------------------+-------------+
-            | str('tiny')       | TINYTEXT    |
-            +-------------------+-------------+
-            | str('medium')     | MEDIUMTEXT  |
-            +-------------------+-------------+
-            | str('long')       | LONGTEXT    |
-            +-------------------+-------------+
             | bytes             | BLOB        |
-            +-------------------+-------------+
-            | bytes(b'tiny')    | TINYBLOB    |
-            +-------------------+-------------+
-            | bytes(b'medium')  | MEDIUMBLOB  |
-            +-------------------+-------------+
-            | bytes(b'long')    | LONGBLOB    |
             +-------------------+-------------+
             | datetime.datetime | DATETIME    |
             +-------------------+-------------+
@@ -158,9 +131,10 @@ class Driver_MySQLConnector(BaseDriver):
 
             .. code-block:: python
 
-                TypeParser.parse(int(8))       # BIGINT
-                TypeParser.parse(str)         # VARCHAR
-                TypeParser.parse(float)       # FLOAT
+                TypeParser.parse(int(10))      # INT DEFAULT 10
+                TypeParser.parse(str)          # VARCHAR
+                TypeParser.parse(float(1.3))   # FLOAT DEFAULT 1.3
+                TypeParser.parse('\t DOUBLE DEFAULT 1.23')    # DOUBLE DEFAULT 1.23
                 ...
 
             """
@@ -168,28 +142,10 @@ class Driver_MySQLConnector(BaseDriver):
 
             supported_types = {
                 bool: 'BOOLEAN',
-
                 int: 'INT',
-                int(1): 'TINYINT',
-                int(2): 'SMALLINT',
-                int(3): 'MEDIUMINT',
-                int(4): 'INT',
-                int(8): 'BIGINT',
-
                 float: 'FLOAT',
-                float(4): 'FLOAT',
-                float(8): 'DOUBLE',
-
                 str: 'VARCHAR(225)',
-                str('char'): 'CHAR',
-                str('tiny'): 'TINYTEXT',
-                str('medium'): 'MEDIUMTEXT',
-                str('long'): 'LONGTEXT',
-                
                 bytes: 'BLOB',
-                bytes(b'tiny'): 'TINYBLOB',
-                bytes(b'medium'): 'MEDIUMBLOB',
-                bytes(b'long'): 'LONGBLOB',
 
                 datetime.datetime: 'DATETIME',
                 datetime.date: 'DATE',
@@ -206,13 +162,21 @@ class Driver_MySQLConnector(BaseDriver):
                 res = supported_types[type_[0]]
 
             # round 2: Custom Types
-            if res == "" and isinstance(type_[0], str):    # custom type
-                return type_[0]
+            if res == "" and isinstance(type_[0], str) and type_[0].startswith('\t'):    # custom type
+                return type_[0].strip()
+            
+            # round 3: Built-in Types With Default Value
+            if type(type_[0]) != type:
+                if isinstance(type_[0], bytes):
+                    type_[0] = type_[0].decode()
+                res = f"{supported_types[type(type_[0])]} DEFAULT {type_[0]}"
 
-            # round 3: Not Null
+            # round 4: Not Null
             for i in range(1, len(type_)):
-                if i == (not None):
+                if type_[i] == 'not null':
                     res += ' NOT NULL'
+                else:
+                    res += f" DFAULT {type_[i]}"
 
             # Not Supported
             if res == "":
@@ -223,7 +187,7 @@ class Driver_MySQLConnector(BaseDriver):
     @staticmethod
     def connect(db_name: str, host: str, user: str, passwd: str = '', force=False) -> Conn:
         """
-        Connect to a 
+        Connect to a MySQL database.
         """
         if force:
             return mysql.connector.connect(
--- tests/drivers/mysql_connector.py
+++ tests/drivers/mysql_connector.py
@@ -1,29 +1,119 @@
 # <--- Test Head --->
-import sys
-sys.path.insert(0, '../../')
-sys.path.insert(0, './')
-
-import io
-SCREEN_OUTPUT = sys.stdout
-TEST_OUTPUT = io.StringIO()
-sys.stdout = TEST_OUTPUT
-
-import re
-from MercurySQL.drivers.mysql import Driver_MySQLConnector
-from MercurySQL import DataBase, set_driver
+if 'set path':
+    import sys
+    sys.path.insert(0, '../../')
+    sys.path.insert(0, './')
+if 'set io':
+    import io
+    SCREEN_OUTPUT = sys.stdout
+    TEST_OUTPUT = io.StringIO()
+    sys.stdout = TEST_OUTPUT
+if 'import':
+    import re
+    from MercurySQL.drivers.mysql import Driver_MySQLConnector
+    from MercurySQL import DataBase, set_driver
 # <--- Test Head End --->
+
 
 # Set the driver to Driver_MySQLConnector
 set_driver(Driver_MySQLConnector)
 
 if __name__ == '__main__':
-    # conn = Driver_MySQLConnector.connect('test', 'localhost', 'test', 'test')
-    # print(Driver_MySQLConnector.APIs.get_all_tables(conn), file=SCREEN_OUTPUT)
-    db = DataBase('test', Driver_MySQLConnector, host='localhost', user='test', passwd='test')
-    # print("1", db.driver.APIs.get_all_tables(db.conn), file=SCREEN_OUTPUT)
-    # print("2", list(db.driver.APIs.get_all_columns(db.conn, 'test')), file=SCREEN_OUTPUT)
-    tb = db['test']
-    # tb['id'] = int, 'primarykey'
-    # tb['name'] = str
-    tb.insert(id=1, name='test', __auto=True)
-    print("1", list(tb.select(tb['id'] == 1)), file=SCREEN_OUTPUT)
+    # Create db
+    db = DataBase("test", host='localhost', user='test', passwd='test')
+    print("Database Information:", db.info)
+
+    # Create table
+    test_table = db['test']
+    print("Tables in the database:", db.tables)
+
+    # Set structure
+    print("Columns in the 'test' table:", test_table.columns)
+    test_table.struct({
+        'id': int,
+        'name': str
+    }, primaryKey='id')
+    test_table['score'] = float
+    test_table['index_'] = int(1), 'primary key', 'auto increment'
+    print("Columns in the 'test' table:", test_table.columns)
+
+    # Access column definition
+    print("Definition of 'id' column:", test_table['id'])
+    print("Definition of 'name' column:", test_table['name'])
+    print(Driver_MySQLConnector.APIs.get_all_columns(db.conn, 'test'))
+
+    # Insert data
+    test_table.insert(id=1, name='test', __auto=True)
+
+    # Query data
+    e = (test_table['id'] == 1) & \
+        (test_table['name'] == 'test')
+    print("Query result:", list(test_table.select(e)))
+
+    # Delete data
+    e.delete()
+    print("After deleting the query result:", list(test_table.select(e)))
+
+    # Delete table
+    del db['test']
+    print("Tables in the database:", db.tables)
+
+
+# <--- Check Test --->
+
+
+def process_output(s):
+    s = s.strip()
+
+    pattern = r' object at 0x[0-9A-Fa-f]+>'
+    s = re.sub(pattern, ' object at 0xPYTHON_ADDRESS>', s)
+
+    return s
+
+
+EXPECTED_OUTPUT = """
+Database Information: {'name': 'test'}
+Tables in the database: {'test': <MercurySQL.core.Table object at 0x000002BC3B3024C0>}
+Columns in the 'test' table: []
+Columns in the 'test' table: ['id', 'name', 'score', 'index_']
+Definition of 'id' column: INT
+Definition of 'name' column: VARCHAR(225)
+[['id', 'int', 'NO', '', None, ''], ['name', 'varchar(225)', 'YES', '', None, ''], ['score', 'float', 'YES', '', None, ''], ['index_', 'int', 'NO', 'PRI', '1', '']]
+Query result: [{'id': 1, 'name': 'test', 'score': None, 'index_': 1}]
+After deleting the query result: []
+Tables in the database: {}
+"""
+
+TEST_OUTPUT = process_output(TEST_OUTPUT.getvalue())
+EXPECTED_OUTPUT = process_output(EXPECTED_OUTPUT)
+
+if TEST_OUTPUT == EXPECTED_OUTPUT:
+    print("\033[32mTest Passed!\033[0m", file=SCREEN_OUTPUT)
+else:
+    print("\033[31mTest Failed!\033[0m", file=SCREEN_OUTPUT)
+    print("Expected Output:", file=SCREEN_OUTPUT)
+    print(EXPECTED_OUTPUT, file=SCREEN_OUTPUT)
+    print('='*30+'\n', file=SCREEN_OUTPUT)
+    print("Test Output:", file=SCREEN_OUTPUT)
+    print(TEST_OUTPUT, file=SCREEN_OUTPUT)
+    print('='*30+'\n', file=SCREEN_OUTPUT)
+    print("Differences:", file=SCREEN_OUTPUT)
+
+    import difflib
+
+    def color_diff(expected, actual):
+        diff = difflib.unified_diff(
+            expected.splitlines(), actual.splitlines(), lineterm='')
+        diff_str = '\n'.join(diff)
+
+        for line in diff_str.splitlines():
+            if line.startswith('+'):
+                print('\033[32m' + line + '\033[0m', file=SCREEN_OUTPUT)  # Green for added lines
+            elif line.startswith('-'):
+                print('\033[31m' + line + '\033[0m', file=SCREEN_OUTPUT)  # Red for removed lines
+            else:
+                print(line, file=SCREEN_OUTPUT)
+
+    color_diff(EXPECTED_OUTPUT, TEST_OUTPUT)
+
+    raise Exception("Test Failed!")
