7bb4480ef4dea680bf6e29984e62a061984f4a01
berniehuang2008 <berniehuang2008@163.com>
2023-12-17 15:29:54 +0800

    Update MySQL driver import and test script

--- MercurySQL/drivers/mysql.py
+++ MercurySQL/drivers/mysql.py
@@ -13,6 +13,10 @@ class Driver_MySQL(BaseDriver):
 
 
 class Driver_MySQL(BaseDriver):
+    """
+    .. note::
+        Supported MySQL Version: **8.2**
+    """
     version = '0.1.0'
     payload = '%s'
 
