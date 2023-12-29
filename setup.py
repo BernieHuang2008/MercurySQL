93cc0ac2f90f74e08fbbadd0597cb5f7c11410da
berniehuang2008 <berniehuang2008@163.com>
2023-12-29 21:00:29 +0800

    Add VERSION file to package

--- setup.py
+++ setup.py
@@ -7,7 +7,9 @@ this_directory = path.abspath(path.dirname(__file__))
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-version = '__!VERSION!__'
+
+with open('MercurySQL/VERSION', 'r') as f:
+    version = f.read().strip()
 
 setup(
     name='MercurySQL',
@@ -19,6 +21,9 @@ setup(
     author_email='berniehuang2008@163.com',
     url='https://github.com/BernieHuang2008/MercurySQL',
     packages=find_packages(),
+    package_data={
+        'MercurySQL': ['VERSION'],
+    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
